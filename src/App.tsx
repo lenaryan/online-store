e6755f40443a6542eb3d5f10cd566dd8d7dd385d
Lena Ryan <alissadyiphil@gmail.com>
2023-06-25 18:15:08 +0600

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import ProductCard from "./components/Card"
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import Cart from "./components/Cart";
+import Order from "./components/Order";
+import ThankYou from "./components/ThankYou";
+import Layout from "./Layout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<MainPage />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/order" element={<Order />} />
+      <Route path="/thank-you" element={<ThankYou />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-      <h1>Store</h1>
-      <ProductCard />
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
---
+++ src/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const Layout = () => (
+    <>
+      <Header />
+      <main><Outlet /></main>
+      <Footer />
+    </>
+  );
+
+  export default Layout;
